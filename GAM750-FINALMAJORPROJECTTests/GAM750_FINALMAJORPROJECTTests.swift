5027bf378d533107d939249429082b554dbf6486
Rachel Saunders <rachelsaundersBA@gmail.com>
2021-02-10 10:23:01 +0000

    Initial Commit

---
+++ GAM750-FINALMAJORPROJECTTests/GAM750_FINALMAJORPROJECTTests.swift
@@ -0,0 +1,33 @@
+//
+//  GAM750_FINALMAJORPROJECTTests.swift
+//  GAM750-FINALMAJORPROJECTTests
+//
+//  Created by Rachel Saunders on 10/02/2021.
+//
+
+import XCTest
+@testable import GAM750_FINALMAJORPROJECT
+
+class GAM750_FINALMAJORPROJECTTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
